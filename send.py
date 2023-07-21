b27d63f6d51ca0b194da43f63ad5765b0c30584a
Natali <nvot@newage.io>
2023-07-21 12:11:27 +0300

    make it works

--- send.py
+++ send.py
@@ -1,27 +1,51 @@
 import asyncio
 import aiohttp
+import time
 
 # Constants
-URL = "http://example.com"  # Replace with your URL
+URL = "https://webhook.site/a3abd1e4-0c0c-41fd-8253-1f609b0427a1"  # Replace with your URL
 DATA = {}  # Replace with your data
-NUM_REQUESTS = 20000
+TOTAL_REQUESTS = 10
+REQUESTS_PER_MINUTE = 5
 SECONDS_IN_MINUTE = 60
 
 # Rate limit
-RATE = NUM_REQUESTS / SECONDS_IN_MINUTE
+RATE = REQUESTS_PER_MINUTE / SECONDS_IN_MINUTE
 
 async def send_request(session):
-    async with session.post(URL, json=DATA) as response:
-        return await response.text()
+    try:
+        print("Sending a request...", time.time())
+        async with session.post(URL, json=DATA) as response:
+            res = await response.text()
+            print(res)
+            return res
+    except Exception as e:
+        print("Error sending request:", e)
 
 async def main():
     tasks = []
     async with aiohttp.ClientSession() as session:
-        for _ in range(NUM_REQUESTS):
-            tasks.append(send_request(session))
-            await asyncio.sleep(1/RATE)  # Rate limit
+        requests_sent = 0
+        start_time = time.time()  # Initialize start_time variable
+        while requests_sent < TOTAL_REQUESTS:
+            for _ in range(REQUESTS_PER_MINUTE):
+                if requests_sent >= TOTAL_REQUESTS:
+                    break
+                task = asyncio.create_task(send_request(session))  # Use asyncio.create_task
+                tasks.append(task)
+                requests_sent += 1
+                if requests_sent % REQUESTS_PER_MINUTE == 0:
+                    elapsed_time = time.time() - start_time
+                    if elapsed_time < SECONDS_IN_MINUTE:
+                        sleep_time = SECONDS_IN_MINUTE - elapsed_time
+                        print("Sleeping for", sleep_time, "seconds")
+                        await asyncio.sleep(sleep_time)
+            if requests_sent < TOTAL_REQUESTS:
+                start_time = time.time()  # Update start_time for the next set
         responses = await asyncio.gather(*tasks)
-        print(responses)  # Do something with the responses
+        print("Total responses:", len(responses))  # Do something with the responses
+        return len(responses)  # Return the total number of responses
 
 # Python 3.7+
-asyncio.run(main())
+total_responses = asyncio.run(main())
+print("Total requests sent:", total_responses)
