b20f589d561cd609eb03354240c3c98f4052083e
Natali <nvot@newage.io>
2023-07-23 19:27:39 +0300

    f

--- send.py
+++ send.py
@@ -1,21 +1,40 @@
 import asyncio
 import aiohttp
 import time
+import uuid
+import random
+import string
 
 # Constants
-URL = "https://webhook.site/a3abd1e4-0c0c-41fd-8253-1f609b0427a1"  # Replace with your URL
-DATA = {}  # Replace with your data
-TOTAL_REQUESTS = 10
-REQUESTS_PER_MINUTE = 5
+URL = "https://stage.track.retainly.app/customers"  # Replace with your URL
+TOTAL_REQUESTS = 10000
+REQUESTS_PER_MINUTE = 10000
 SECONDS_IN_MINUTE = 60
 
 # Rate limit
 RATE = REQUESTS_PER_MINUTE / SECONDS_IN_MINUTE
 
+async def generate_random_data():
+    customer_id = str(uuid.uuid4())  # Generate a random UUID
+    email = ''.join(random.choices(string.ascii_lowercase, k=10)) + "@example.com"  # Random email
+    phone = ''.join(random.choices(string.digits, k=10))  # Random phone number
+    return {
+        "id": customer_id,
+        "email": email,
+        "phone": phone
+    }
+
 async def send_request(session):
     try:
+        headers = {
+            "key": "zA1LzpS9HMRJ6uoUVcCa2DEPfbe78N7JWe5SCzRmmtKLn6EZntoRk4h3dfSh",  # Replace with your key
+            "secret": "i5GMURViZ8VZ9MRSpy60DPBFwnLJ1y0bjZ0pQHX0JJEAthbuqwdxXZYUN8MI"  # Replace with your secret
+        }
+        data = {
+            "customers": [await generate_random_data()]
+        }
         print("Sending a request...", time.time())
-        async with session.post(URL, json=DATA) as response:
+        async with session.post(URL, json=data, headers=headers) as response:
             res = await response.text()
             print(res)
             return res
